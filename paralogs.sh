fea02393dde911e77b9c8889805bf5699caf6a0e
Andrew <andrew.j.helmstetter@gmail.com>
2018-09-10 15:41:40 +0200

    paralog script edits

--- paralogs.sh
+++ paralogs.sh
@@ -57,6 +57,24 @@ module load bioinfo/mafft/7.305
 
 cd $path_to_tmp
 
+#Previously par_table.sh 
+#Finds all genes with warnings from hybpiper run and makes a list 
+
+find . -name genes_with_paralog_warnings.txt >> par_list.txt
+
+rm para_table.txt
+touch para_table.txt
+
+while read i
+do
+cat $i >> para_table.txt
+echo $i >> para_table.txt
+
+echo -e "\n" >> para_table.txt
+done < par_list.txt
+
+sed -i '/^$/d' para_table.txt
+
 cat para_table.txt | sort -f | uniq > loci_list.txt
 
 while read i
