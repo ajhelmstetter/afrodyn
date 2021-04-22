118550432e88e73038765ff2232a29c8303a309c
Andrew Helmstetter <andrew.j.helmstetter@gmail.com>
2021-04-22 11:54:05 +0200

    improving parallelization of hybpiper.sh

--- cluster_scripts/hybpiper.sh
+++ cluster_scripts/hybpiper.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
-############      SGE CONFIGURATION      ###################
-# Ecrit les erreur dans le fichier de sortie standard 
-#$ -j y 
-
-# Shell que l'on veut utiliser 
-#$ -S /bin/bash 
-
-# Email pour suivre l'execution 
-#$ -M andrew.j.helmstetter@gmail.com
-
-# Type de massage que l'on recoit par mail
-#    -  (b) un message au demarrage
-#    -  (e) a la fin
-#    -  (a)  en cas d'abandon
-#$ -m e 
-#$ -V
-
-# Queue que l'on veut utiliser
-#$ -q bioinfo.q
-
-# Nom du job
-#$ -N hybpiper
+############      SLURM CONFIGURATION      ###################
+#SBATCH --job-name=hybiper_magnoliids
+#SBATCH --cpus-per-task=1 
+#SBATCH --ntasks-per-node=8
+#SBATCH --mail-user=andrew.j.helmstetter@gmail.com
+#SBATCH --mail-type=ALL
 ############################################################
+
+echo "JOB CONFIGURATION"
+echo "Job ID: " $SLURM_JOB_ID
+echo "Name of the job: " $SLURM_JOB_NAME		
+echo "List of nodes allocated to the job: " $SLURM_JOB_NODELIST
+echo "Number of nodes allocated to the job: " $SLURM_JOB_NUM_NODES
+echo "Number of CPU tasks in this job: " $SLURM_NTASKS
+echo "Directory from which sbatch was invoked: " $SLURM_SUBMIT_DIR
 
 ###################################################
 #### 0 Preparation of files and transfer to cluster
@@ -34,33 +26,45 @@
 
 #Files that are in $path_to_dir_in:
 #Reference file
-#Annonaceae_nuc_exons.fa
+#Angiosperms353_targetSequences.fasta
 #namelist.txt #### namelist contains the sample names that will be analysed
-path_to_dir_in="/home/helmstetter/hybpiper/data";
+path_to_dir_in="/data3/projects/home/helmstetter/data";
 
 #this is where the hybpiper folder is located after download
 #https://github.com/mossmatters/HybPiper
-path_to_hybpiper="/home/helmstetter/hybpiper/program";
+path_to_hybpiper="/data3/projects/home/helmstetter/hybpiper/program";
 
 #The 'scripts' folder contains scripts for running intronerate
 #and generating summary stats:
 #hybpiper_stats.sh OR hybpiper_stats_palm.sh if working with palms 
 #intronerate.sh
 #get_seq_lengths.sh
-path_to_scripts="/home/helmstetter/hybpiper/scripts";
+path_to_scripts="/data3/projects/home/helmstetter/hybpiper/scripts";
 
 #output folder
-path_to_dir_out="/home/helmstetter/output/$JOB_ID/";
+path_to_dir_out="/data3/projects/home/helmstetter/hybpiper_magnoliids_$SLURM_JOB_ID/";
 
 #temporary folder (intermediate files)
-path_to_tmp="/scratch/helmstetter_$JOB_ID";
+path_to_tmp="/scratch/helmstetter_$SLURM_JOB_ID";
 
 #make temporary directory to store files/run analyses in
+
 echo "copying files";
 mkdir $path_to_tmp
 
 #copy refrence files and namelist to temp directory
-scp nas:/$path_to_dir_in/* $path_to_tmp
+scp nas3:$path_to_dir_in/* $path_to_tmp
+
+#Copy scripts that are used in the pipeline to temporary folder
+echo "copying scripts";
+
+#copy all hybpiper python scripts
+scp nas3:/$path_to_hybpiper/*.py $path_to_tmp
+
+#Copy shell scripts
+scp nas3:/$path_to_scripts/*.sh $path_to_tmp
+
+echo "done copying scripts";
 
 #Copy all raw fastq files needed
 #Change appropriately for the files that are required
@@ -70,44 +74,46 @@ echo "copying fastqs";
 # INSERT PATHS TO RAW FASTQs #
 ##############################
 
-scp nas2:/data/projects/afrodyn/RUN62_HISEQ/paired/INDEX12/* $path_to_tmp
-scp nas2:/data/projects/afrodyn/RUN52/paired/trimtfiltcutR52-TAG-71* $path_to_tmp
-scp nas2:/data/projects/afrodyn/RUN60_HISEQ/paired/trimtfiltcutRUN60_HISEQ-INDEX02-TAG-62* $path_to_tmp
+#copy all
+#scp nas3:/data3/projects/AFRODYN2/magnoliids/*fastq.gz $path_to_tmp
+
+mv $path_to_tmp/namelist_magnoliids.txt $path_to_tmp/namelist.txt
+
+#move to scratch directory
+cd $path_to_tmp
+
+rm copy_parallel.txt
+touch copy_parallel.txt
+
+while read name;
+do
+	echo "scp nas3:/data3/projects/AFRODYN2/magnoliids/${name}*.gz $path_to_tmp" >> copy_parallel.txt
+done < namelist.txt
+
+#run jobs in lots of4
+parallel -j 4 < copy_parallel.txt
 
 echo "done copying fastqs";
 
-#Copy scripts that are used in the pipeline to temporary folder
-echo "copying scripts";
-
-#copy all hybpiper python scripts
-scp nas:/$path_to_hybpiper/*.py $path_to_tmp
-
-#Copy shell scripts
-scp nas:/$path_to_scripts/*.sh $path_to_tmp
-
-echo "done copying scripts";
-
 echo "done copying all files";
 
-#Change directory to temporary directory to gunzip and rename fastqs
-cd $path_to_tmp
+# gunzip and rename fastqs
 
-#Gunzip files
-#takes a while if you have many
 echo "gunzipping files"
-gunzip *fastq.gz
+ls *.gz | parallel -j 8 gunzip 
 echo "done gunzipping files"
 
 #Renames to the format e.g.I04_T44_R1
 #made need to change patterns depending on run used
 #this rename works on IRD cluster but different linux OS may require changes
 echo "renaming files"
-rename -v 'trimtfiltcutRUN62_HISEQ_INDEX' 'I' *
-rename -v 'trimtfiltcutRUN60_HISEQ-INDEX' 'I' *
-rename -v 'trimtfiltcutR52' 'R52' *
-rename -v -- '-TAG-' '_T' *
-rename -v '_paired' '' *
+
+#rename -v '_paired' '' *
+
 echo "done renaming files"
+
+echo "LIST OF THE FILES:"
+ls
 
 #load modules
 module load bioinfo/SPAdes
@@ -118,12 +124,16 @@ module load bioinfo/SPAdes
 
 echo "starting reads_first";
 
-mv $path_to_tmp/namelist_GROUPNAME.txt $path_to_tmp/namelist.txt
+rm hybpiper_parallel.txt
+touch hybpiper_parallel.txt
 
-#A loop that reads names of samples in namelist and runs reads_first.py part of the hybpiper pipeline
-while read name; 
-do python $path_to_tmp/reads_first.py -b $path_to_tmp/Annonaceae_nuc_exons.fa -r $path_to_tmp/"$name"_R1.fastq $path_to_tmp/"$name"_R2.fastq --prefix $path_to_tmp/$name --bwa
-done < $path_to_tmp/namelist.txt
+while read name;
+do
+	echo "python $path_to_tmp/reads_first.py -b $path_to_tmp/Angiosperms353_targetSequences.fasta -r $path_to_tmp/${name}_R1.fastq $path_to_tmp/${name}_R2.fastq --prefix $path_to_tmp/$name --cpu 1 --bwa" >> hybpiper_parallel.txt
+done < namelist.txt
+
+#run jobs in lots of 8
+parallel -j 8 < hybpiper_parallel.txt
 
 echo "done reads_first";
 
@@ -167,15 +177,16 @@ done < namelist.txt
 # moves files to relevant folders
 
 mkdir retrieved_exons
-python retrieve_sequences.py Annonaceae_nuc_exons.fa . dna
+python retrieve_sequences.py Angiosperms353_targetSequences.fasta . dna
 mv *.FNA retrieved_exons/
 
 mkdir retrieved_introns
-python retrieve_sequences.py Annonaceae_nuc_exons.fa . intron
+python retrieve_sequences.py Angiosperms353_targetSequences.fasta . intron
 mv *.fasta retrieved_introns/
 
 mkdir retrieved_supercontigs
-python retrieve_sequences.py Annonaceae_nuc_exons.fa . supercontig
+mv retrieved_introns/Angiosperms353_targetSequences.fasta ./
+python retrieve_sequences.py Angiosperms353_targetSequences.fasta . supercontig
 mv *.fasta retrieved_supercontigs/
 
 #This keeps all files related to paralog warnings
@@ -187,6 +198,8 @@ while read i
 do
 mkdir -p retrieved_par/$i && cp $i retrieved_par/$i
 done < para.txt
+
+# returns weird warnings like "cp: ommission du répertoire" for every directory, but the copy did work for the files
 
 ##################################################
 #### 6 clean up and transfer
@@ -201,14 +214,14 @@ cd ~
 mkdir $path_to_dir_out
 
 #fastq files can take up a lot of space so you may want to remove them
-#rm $path_to_tmp/*.fastq
+rm $path_to_tmp/*.fastq
 
 #Copies statistics and retrieved sequences to output folder in home directory
-scp -p $path_to_tmp/*.txt nas:/$path_to_dir_out/
-scp -rp $path_to_tmp/retrieved_exons/ nas:/$path_to_dir_out/
-scp -rp $path_to_tmp/retrieved_introns/ nas:/$path_to_dir_out/
-scp -rp $path_to_tmp/retrieved_supercontigs/ nas:/$path_to_dir_out/
-scp -rp $path_to_tmp/retrieved_par/ nas:/$path_to_dir_out/
+scp -p $path_to_tmp/*.txt nas3:/$path_to_dir_out/
+scp -rp $path_to_tmp/retrieved_exons/ nas3:/$path_to_dir_out/
+scp -rp $path_to_tmp/retrieved_introns/ nas3:/$path_to_dir_out/
+scp -rp $path_to_tmp/retrieved_supercontigs/ nas3:/$path_to_dir_out/
+scp -rp $path_to_tmp/retrieved_par/ nas3:/$path_to_dir_out/
 
 #copy everything, for testing
 #scp -rp $path_to_tmp/* nas:/$path_to_dir_out/
@@ -217,6 +230,10 @@ echo "done moving";
 
 #### Delete all data on node to keep space free
 
-echo "Deleting data on node";
-rm -rf $path_to_tmp
-echo "Done deleting, FINISHED!";
+#echo "Deleting data on node";
+#rm -rf $path_to_tmp
+#echo "Done deleting, FINISHED!";
+
+echo "Data NOT deleted on node, FINISHED"
+echo "Must delete the folder $path_to_tmp on $SLURM_JOB_NODELIST"
+
